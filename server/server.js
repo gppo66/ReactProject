333ce6d1c3c541a966324ef914c3e1da8e8daac2
gppo66 <gppo66@gmail.com>
2021-06-29 18:42:37 +0900

    node setting using middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 import app from './app';
 
-app.listen('7000', () => {
-  console.log('hi');
+import config from './config/index';
+
+const { PORT } = config;
+
+app.listen(PORT, () => {
+  console.log(`Server stated on Port ${PORT}`);
 });
