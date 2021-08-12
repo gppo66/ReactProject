6ca18c8686893e98f4612adc421b922ed6ea43d3
gppo66 <gppo66@gmail.com>
2021-08-12 19:36:13 +0900

    error fixing

--- client/src/routes/nomalRoute/PostWrite.js
+++ client/src/routes/nomalRoute/PostWrite.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {} from '@ckeditor/ckeditor5-react';
 import {} from '@ckeditor/ckeditor5-build-classic';
+
 const PostWrite = () => {
   return <h1>PostWrite</h1>;
 };
